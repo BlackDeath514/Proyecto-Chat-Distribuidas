2429e1748f9eb61318311f28952ce9ffe29cc059
BlackDeath514 <188209233+BlackDeath514@users.noreply.github.com>
2026-07-06 12:15:13 -0500

    Configurar frontend para produccion

--- chat-frontend/src/pages/Chat.jsx
+++ chat-frontend/src/pages/Chat.jsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react'
 import api from '../services/api'
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000')
+
+
+const socket = io(
+  'https://proyecto-chat-distribuidas.onrender.com'
+)
 
 function Chat() {
 
